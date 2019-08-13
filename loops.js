818849927b1cbd758f51c43ddfbefb0d34f01c3c
Leon Dail <leondailofficial@gmail.com>
2019-08-13 14:12:05 -0700

    for loop finish

---
+++ loops.js
@@ -0,0 +1,18 @@
+'use strict';
+
+function multiplyAll(arr) {
+  let product = 1;
+  // Only change code below this line
+  for (let i=0;i<arr.length;i++) {
+    for (let j=0;j<arr[i].length;j++) {
+      product *= arr[i][j];
+    }
+  }
+  
+  // Only change code above this line
+  return product;
+}
+  
+// Modify values below to test your code
+multiplyAll([[1,2],[3,4],[5,6,7]]);
+  
